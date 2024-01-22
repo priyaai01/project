d54f0bbad5ebd874229dee78d777220ee68a936f
lpriyaai01 <lpriyaai01@gmail.com>
2024-01-22 11:03:17 +0530

    added files

---
+++ c++/constructedprogram.c++/main.cpp
@@ -0,0 +1,81 @@
+/*#include<iostream>
+using namespace std;
+
+ class constructed
+ {
+ public:
+     int a,b;
+
+    constructed()
+    {
+    a=12;
+    b=45;
+    }
+
+};
+ int main()
+ {
+     constructed cot;
+     cout<<"a:"<<cot.a<<endl<<"b:"<<cot.b;
+     return 0;
+ }*/
+
+
+
+/* #include<iostream>
+ using namespace std;
+
+ class constructed
+ {
+ public:
+    int a,b;
+
+    constructed()
+    {
+        cout<<"Enter value a:";
+        cin>>a;
+        cout<<"Enter value b:";
+        cin>>b;
+        cout<<(a+b);
+    }
+
+ };
+ int main()
+ {
+     constructed cot;
+     return 0;
+ }*/
+
+
+
+ /*#include<iostream>
+ using namespace std;
+ class school
+ {
+ public:
+    int reg;
+    char name[50];
+    int age;
+
+    school()
+    {
+
+        cout<<"reg:";
+        cin>>reg;
+        cout<<"name:";
+        cin>>name;
+        cout<<"age:";
+        cin>>age;
+
+    }
+
+ };
+ int main()
+ {
+     school scl;
+     return 0;
+ }*/
+
+
+
+
