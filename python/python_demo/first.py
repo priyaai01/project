57c02aff138fd275182b2c94aef036cbfe16e2e4
Priya S <priyaai01@brightskills.com>
2024-02-06 11:04:07 +0530

    added files

---
+++ python/python_demo/first.py
@@ -0,0 +1,59 @@
+a=56;
+b=32;
+c=a+b;
+print(c);
+print(type(c));
+c=a-b;
+print(c);
+c=a/b;
+print(c);
+
+
+first=67;
+second="priya";
+print(first);
+print(second);
+print(type(first));
+print(type(second));
+
+
+a=5;
+b=67;
+c=a+b;
+print("enter the value a:",c);
+if (a>=b):
+	print("Greater value")
+elif(a<=b):
+	print("Less value")
+
+
+a,b=input("Enter the two value:");
+print("The number of dogs:",a);
+print("The number of elephant:",b);
+
+
+print("ENTER THE VALUE V:");
+v=input(v);
+
+print("ENTER THE VALUE S:");
+s=input(s);
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
---
+++ python/python_demo/second.py
@@ -0,0 +1,34 @@
+'''a=input("sillunu oru kadhal")
+
+if(a=="died"):
+    print("gowtham meets gundhavi")
+else:
+    print("gowtham weeds aishu")'''
+
+'''a=int(input())
+b=int(input())
+c=int(input())
+d=a+b+c
+print("Add value",d)
+e=a*b*c
+print("Multi value",e)
+f=d/b
+print("Div value",f)'''
+
+mark=int(input("school mark"))
+
+if(mark>35):
+    print("pass")
+else:
+    print("fail")
+
+
+
+ 
+
+
+
+
+
+
+
